93c04d0e8f5c5e3e814c207cdc65938542a4d151
Hira-Haider-46 <haiderhira46@gmail.com>
2025-03-04 21:03:00 +0500

    flight deals component updated

--- src/components/FlightDeals.tsx
+++ src/components/FlightDeals.tsx
@@ -2,6 +2,7 @@ import { FaArrowRight } from "react-icons/fa";
 import card_1 from "../assets/card_1.png";
 import card_2 from "../assets/card_2.png";
 import card_3 from "../assets/card_3.png";
+import card_4 from "../assets/card_7.png";
 
 const FlightDeals: React.FC = () => {
   return (
@@ -17,22 +18,61 @@ const FlightDeals: React.FC = () => {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {[
-          { img: card_1, place: "The Bund", location: "Shanghai", price: "$598", desc: "China's most international city" },
-          { img: card_2, place: "Sydney Opera House", location: "Sydney", price: "$981", desc: "Take a stroll along the famous harbor" },
-          { img: card_3, place: "Kōdaiji Temple", location: "Kyoto", price: "$633", desc: "Step back in time in the Gion district" }
+          {
+            img: card_1,
+            place: "The Bund",
+            location: "Shanghai",
+            price: "$598",
+            desc: "China's most international city",
+          },
+          {
+            img: card_2,
+            place: "Sydney Opera House",
+            location: "Sydney",
+            price: "$981",
+            desc: "Take a stroll along the famous harbor",
+          },
+          {
+            img: card_3,
+            place: "Kōdaiji Temple",
+            location: "Kyoto",
+            price: "$633",
+            desc: "Step back in time in the Gion district",
+          },
         ].map((deal, index) => (
-          <div 
-            key={index} 
-            className="rounded-xl shadow-lg cursor-pointer hover:scale-101 transition-transform duration-300 ease-in-out w-[80%] sm:w-[85%] lg:w-[95%] mx-auto"
+          <div
+            key={index}
+            className="rounded-xl shadow-lg cursor-pointer hover:scale-99 duration-500 w-[80%] sm:w-[85%] lg:w-[95%] mx-auto"
           >
-            <img src={deal.img} alt={deal.place} className="rounded-t-xl w-full h-48 object-cover xl:h-auto" />
+            <img
+              src={deal.img}
+              alt={deal.place}
+              className="rounded-t-xl w-full h-48 object-cover xl:h-auto"
+            />
             <div className="flex justify-between items-center px-4 pt-4 font-semibold">
-              <p>{deal.place}, <span className="text-indigo-500">{deal.location}</span></p>
+              <p>
+                {deal.place},{" "}
+                <span className="text-indigo-500">{deal.location}</span>
+              </p>
               <p>{deal.price}</p>
             </div>
             <p className="ml-4 mb-5 mt-1 text-sm">{deal.desc}</p>
           </div>
         ))}
+      </div>
+      <div className="rounded-xl shadow-lg cursor-pointer hover:scale-99 duration-500 w-[80%] mx-auto md:w-full my-10 pb-4.5">
+        <img
+          src={card_4}
+          alt='card_4'
+          className="rounded-t-xl w-full h-48 object-cover xl:h-auto"
+        />
+        <div className="flex justify-between items-center px-4 pt-4 font-semibold">
+          <p>
+          Tsavo East National Park, <span className="text-indigo-500">Kenya</span>
+          </p>
+          <p>$1,248</p>
+        </div>
+        <p className="ml-4 mt-1 text-sm">Named after the Tsavo River, and opened in April 1984, Tsavo East National Park is one of the oldest parks in Kenya. It is located in the semi-arid Taru Desert.</p>
       </div>
     </div>
   );

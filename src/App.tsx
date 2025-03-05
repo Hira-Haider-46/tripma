c39336b6eb19f08e26c3d1396380bc09d48cefb7
Hira-Haider-46 <haiderhira46@gmail.com>
2025-03-05 16:29:50 +0500

    flights details component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import Navbar from "./components/Navbar";
-import SearchFlight from "./components/SearchFlight";
-import FlightDeals from "./components/FlightDeals";
-import Comments from "./components/Comments";
+import FlightsDetails from "./components/FlightsDetails";
 import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
-      <SearchFlight />
-      <FlightDeals />
-      <Comments />
+      <FlightsDetails />
       <Footer />
     </>
   );
---
+++ src/components/FlightsDetails.tsx
@@ -0,0 +1,65 @@
+import {
+  FaPlaneDeparture,
+  FaPlaneArrival,
+  FaCalendarAlt,
+  FaUsers,
+} from "react-icons/fa";
+
+const FlightsDetails: React.FC = () => {
+  return (
+    <div className="p-10 flex flex-row">
+      <div className="w-3/5">
+        <div className="w-full mx-auto flex flex-wrap lg:flex-nowrap items-center gap-4 lg:gap-0 text-gray-500 lg:shadow-xs text-sm">
+          <div className="relative flex items-center w-full lg:w-64 border border-gray-300 px-3 py-2.5 lg:rounded-tl-sm lg:rounded-bl-sm bg-white rounded-sm lg:rounded-none">
+            <FaPlaneDeparture className="absolute left-2.75 w-3.5 h-3.5" />
+            <input
+              placeholder="From where?"
+              className="pl-7 pr-4 w-full outline-none"
+            />
+          </div>
+          <div className="relative flex items-center w-full lg:w-64 border border-gray-300 px-3 py-2.5 bg-white rounded-sm lg:rounded-none">
+            <FaPlaneArrival className="absolute left-2.75 w-3.5 h-3.5" />
+            <input
+              placeholder="To where?"
+              className="pl-7 pr-4 w-full outline-none"
+            />
+          </div>
+          <div className="relative flex items-center w-full lg:w-64 border border-gray-300 px-3 py-2.5 bg-white rounded-sm lg:rounded-none">
+            <FaCalendarAlt className="absolute left-2.75 w-3.5 h-3.5" />
+            <input
+              type="text"
+              placeholder="Depart - Return"
+              className="pl-7 pr-4 w-full outline-none"
+            />
+          </div>
+          <div className="relative flex items-center w-full lg:w-64 border border-gray-300 px-3 py-2.5 bg-white rounded-sm lg:rounded-none">
+            <FaUsers className="absolute left-2.75 w-3.5 h-3.5" />
+            <input
+              placeholder="Passengers"
+              className="pl-7 pr-4 w-full outline-none"
+            />
+          </div>
+          <button className="border-1 border-indigo-500 w-full lg:w-32 bg-indigo-500 text-white rounded-sm px-6 py-2.5 cursor-pointer hover:scale-101">
+            Search
+          </button>
+        </div>
+        <div className="my-5 text-sm w-[80%] text-gray-700 flex flex-wrap gap-4 sm:grid sm:grid-cols-2 md:flex md:flex-row lg:flex-nowrap">
+          {["Max Price", "Shops", "Times", "Airlines", "Seat class"].map(
+            (label, index) => (
+              <div
+                key={index}
+                className="w-full w-[48%] md:w-[18.5%] border border-gray-300 text-center py-2 rounded-sm"
+              >
+                <select className="border-none outline-none w-[85%] bg-transparent">
+                  <option value="">{label}</option>
+                </select>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlightsDetails;

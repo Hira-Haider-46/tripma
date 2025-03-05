8953ae174b21e96f972886b137638a78fea6fd4f
Hira-Haider-46 <haiderhira46@gmail.com>
2025-03-05 09:59:41 +0500

    flight deals component updated

--- src/components/FlightDeals.tsx
+++ src/components/FlightDeals.tsx
@@ -3,6 +3,9 @@ import card_1 from "../assets/card_1.png";
 import card_2 from "../assets/card_2.png";
 import card_3 from "../assets/card_3.png";
 import card_4 from "../assets/card_7.png";
+import card_5 from "../assets/card_4.png";
+import card_6 from "../assets/card_5.png";
+import card_7 from "../assets/card_6.png";
 
 const FlightDeals: React.FC = () => {
   return (
@@ -12,7 +15,7 @@ const FlightDeals: React.FC = () => {
           Find your next adventure with these{" "}
           <span className="text-indigo-500">flight deals</span>
         </h1>
-        <p className="flex items-center gap-2 text-lg cursor-pointer text-gray-400 hover:text-indigo-500 transition">
+        <p className="flex items-center gap-2 text-lg cursor-pointer text-gray-400">
           All <FaArrowRight />
         </p>
       </div>
@@ -73,6 +76,55 @@ const FlightDeals: React.FC = () => {
           <p>$1,248</p>
         </div>
         <p className="ml-4 mt-1 text-sm">Named after the Tsavo River, and opened in April 1984, Tsavo East National Park is one of the oldest parks in Kenya. It is located in the semi-arid Taru Desert.</p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8">
+            <h1 className="font-semibold text-2xl mb-4 md:mb-0">
+            Explore unique{" "}
+              <span className="text-cyan-400">places to stay</span>
+            </h1>
+            <p className="flex items-center gap-2 text-lg cursor-pointer text-gray-400">
+              All <FaArrowRight />
+            </p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {[
+          {
+            img: card_5,
+            place: "Stay among the atolls in",
+            location: "Maldives",
+            desc: "From the 2nd century AD, the islands were known as the 'Money Isles' due to the abundance of cowry shells, a currency of the early ages.",
+          },
+          {
+            img: card_7,
+            place: "Experience the Ourika Valley in",
+            location: "Morocco",
+            desc: "Morocco’s Hispano-Moorish architecture blends influences from Berber culture, Spain, and contemporary artistic currents in the Middle East.",
+          },
+          {
+            img: card_6,
+            place: "Live traditionally in",
+            location: "Mongolia",
+            desc: "Traditional Mongolian yurts consists of an angled latticework of wood or bamboo for walls, ribs, and a wheel.",
+          },
+        ].map((deal, index) => (
+          <div
+            key={index}
+            className="rounded-xl shadow-lg cursor-pointer hover:scale-99 duration-500 w-[80%] sm:w-[85%] lg:w-[95%] mx-auto"
+          >
+            <img
+              src={deal.img}
+              alt={deal.place}
+              className="rounded-t-xl w-full h-48 object-cover xl:h-auto"
+            />
+            <div className="px-5 pt-4">
+              <p className="font-semibold">
+                {deal.place},{" "}
+                <span className="text-cyan-400">{deal.location}</span>
+              </p>
+              <p className="mb-5 mt-1 text-sm">{deal.desc}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
